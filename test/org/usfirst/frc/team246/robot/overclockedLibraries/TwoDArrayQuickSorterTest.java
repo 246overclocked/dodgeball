a5bf20039d88a7c0c2764e7d82d06f09a87563a1
jnazaren <jnazaren@gmail.com>
2015-12-30 15:18:39 -0500

    added some tests for TwoDArrayQuickSorter, including null and invalid argument handling

--- test/org/usfirst/frc/team246/robot/overclockedLibraries/TwoDArrayQuickSorterTest.java
+++ test/org/usfirst/frc/team246/robot/overclockedLibraries/TwoDArrayQuickSorterTest.java
@@ -37,6 +37,22 @@ public class TwoDArrayQuickSorterTest {
 	}
 	
 	@Test
+	public void testQuickSortLong() {
+		double[][] array = new double[ARRAY_SIZE][];
+		for (int i = 0; i < ARRAY_SIZE; i++) {
+			int l = randomNumbers.nextInt(10) + 5;
+			array[i] = new double[l];
+			for (int x = 0; x < l; x++) {
+				array[i][x] = randomNumbers.nextInt(100);
+			}
+		}
+		quickSorter.quickSort(array, 4);
+		for (int i = 0; i < ARRAY_SIZE-1; i++) {
+			Assert.assertTrue(array[i][4] <= array[i+1][4]);
+		}
+	}
+	
+	@Test
 	public void testQuickSortConsistency() {
 		double[][] array = new double[ARRAY_SIZE][2];
 		for (int i = 0; i < ARRAY_SIZE; i++) {
@@ -50,6 +66,30 @@ public class TwoDArrayQuickSorterTest {
 		quickSorter.quickSort(array, 0);
 		quickSorter.quickSort(array2, 1);
 		Assert.assertTrue(Arrays.deepEquals(array, array2));
+	}
+	
+	@Test (expected = NullPointerException.class)
+	public void testNullArrayHandeling() {
+		double[][] array = null;
+		quickSorter.quickSort(array, 0);
+	}
+	
+	@Test
+	public void testShortArrays() {
+		double[][] array = {};
+		double[][] arrayCopy = {};
+		double[][] array2 = {{0,0}};
+		double[][] array2Copy = {{0,0}};
+		quickSorter.quickSort(arrayCopy, 0);
+		quickSorter.quickSort(array2Copy, 0);
+		Assert.assertTrue(Arrays.deepEquals(array, arrayCopy));
+		Assert.assertTrue(Arrays.deepEquals(array2, array2Copy));
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testInvalidIndex() {
+		double[][] array = {{0,0},{1,1,1},{2,2},{3,3},{4,4},{5,5}};
+		quickSorter.quickSort(array, 2);
 	}
 
 }
