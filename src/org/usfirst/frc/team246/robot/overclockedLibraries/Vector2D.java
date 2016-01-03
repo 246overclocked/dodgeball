95b95d6c4235e5e8a4612a8fa8371f35ba39fd32
Michael Silver <michaelssilver@me.com>
2016-01-03 18:46:54 -0500

    Make polarToCart() more concise

--- src/org/usfirst/frc/team246/robot/overclockedLibraries/Vector2D.java
+++ src/org/usfirst/frc/team246/robot/overclockedLibraries/Vector2D.java
@@ -60,8 +60,7 @@ public class Vector2D {
 	 */
 	public static double[] polarToCart(double r, double theta){
     	theta += 90;
-        double[] cartCoords = {r*Math.cos(Math.toRadians(theta)), r*Math.sin(Math.toRadians(theta))};
-        return cartCoords;
+        return new double[] {r*Math.cos(Math.toRadians(theta)), r*Math.sin(Math.toRadians(theta))};
     }
     
 	/**
